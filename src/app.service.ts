86ca4377a0de2c418e9ffafd0e62841b3fdb1b9f
George Salamanca <GSalamanca@leggmason.com>
2020-11-03 15:28:27 -0500

    removed failing test

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): { ret: string } {
-    return { ret: 'Hello World!' };
+  getHello(): string {
+    return 'Hello World!';
   }
 }
